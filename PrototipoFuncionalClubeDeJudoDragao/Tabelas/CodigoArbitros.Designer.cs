f31f45300f6cb5e696757963ede330c55feb8351
Gone4Coding <gongas.gordalina14@gmail.com>
2016-07-06 11:02:09 +0100

    Codigo Arbitros e Entidades

---
+++ PrototipoFuncionalClubeDeJudoDragao/Tabelas/CodigoArbitros.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace PrototipoFuncionalClubeDeJudoDragao.Tabelas
+{
+    partial class CodigoArbitros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bt_last = new System.Windows.Forms.Button();
+            this.bt_previous = new System.Windows.Forms.Button();
+            this.bt_next = new System.Windows.Forms.Button();
+            this.bt_first = new System.Windows.Forms.Button();
+            this.tb_numeroArbitros = new System.Windows.Forms.TextBox();
+            this.bt_novo = new System.Windows.Forms.Button();
+            this.bt_alterar = new System.Windows.Forms.Button();
+            this.bt_eliminar = new System.Windows.Forms.Button();
+            this.bt_ok = new System.Windows.Forms.Button();
+            this.bt_cancelar = new System.Windows.Forms.Button();
+            this.bt_pesquisa = new System.Windows.Forms.Button();
+            this.bt_imprimir = new System.Windows.Forms.Button();
+            this.bt_sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código do Árbitro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Descrição";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(61, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // bt_last
+            // 
+            this.bt_last.Location = new System.Drawing.Point(12, 105);
+            this.bt_last.Name = "bt_last";
+            this.bt_last.Size = new System.Drawing.Size(65, 23);
+            this.bt_last.TabIndex = 2;
+            this.bt_last.Text = "Último";
+            this.bt_last.UseVisualStyleBackColor = true;
+            // 
+            // bt_previous
+            // 
+            this.bt_previous.Location = new System.Drawing.Point(83, 105);
+            this.bt_previous.Name = "bt_previous";
+            this.bt_previous.Size = new System.Drawing.Size(65, 23);
+            this.bt_previous.TabIndex = 2;
+            this.bt_previous.Text = "Anterior";
+            this.bt_previous.UseVisualStyleBackColor = true;
+            // 
+            // bt_next
+            // 
+            this.bt_next.Location = new System.Drawing.Point(154, 105);
+            this.bt_next.Name = "bt_next";
+            this.bt_next.Size = new System.Drawing.Size(65, 23);
+            this.bt_next.TabIndex = 2;
+            this.bt_next.Text = "Seguinte";
+            this.bt_next.UseVisualStyleBackColor = true;
+            // 
+            // bt_first
+            // 
+            this.bt_first.Location = new System.Drawing.Point(225, 106);
+            this.bt_first.Name = "bt_first";
+            this.bt_first.Size = new System.Drawing.Size(65, 22);
+            this.bt_first.TabIndex = 2;
+            this.bt_first.Text = "Primeiro";
+            this.bt_first.UseVisualStyleBackColor = true;
+            // 
+            // tb_numeroArbitros
+            // 
+            this.tb_numeroArbitros.Location = new System.Drawing.Point(297, 107);
+            this.tb_numeroArbitros.Name = "tb_numeroArbitros";
+            this.tb_numeroArbitros.Size = new System.Drawing.Size(55, 20);
+            this.tb_numeroArbitros.TabIndex = 3;
+            // 
+            // bt_novo
+            // 
+            this.bt_novo.Location = new System.Drawing.Point(388, 13);
+            this.bt_novo.Name = "bt_novo";
+            this.bt_novo.Size = new System.Drawing.Size(75, 23);
+            this.bt_novo.TabIndex = 5;
+            this.bt_novo.Text = "Novo";
+            this.bt_novo.UseVisualStyleBackColor = true;
+            // 
+            // bt_alterar
+            // 
+            this.bt_alterar.Location = new System.Drawing.Point(388, 42);
+            this.bt_alterar.Name = "bt_alterar";
+            this.bt_alterar.Size = new System.Drawing.Size(75, 23);
+            this.bt_alterar.TabIndex = 5;
+            this.bt_alterar.Text = "Alterar";
+            this.bt_alterar.UseVisualStyleBackColor = true;
+            // 
+            // bt_eliminar
+            // 
+            this.bt_eliminar.Location = new System.Drawing.Point(388, 71);
+            this.bt_eliminar.Name = "bt_eliminar";
+            this.bt_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.bt_eliminar.TabIndex = 5;
+            this.bt_eliminar.Text = "Eliminar";
+            this.bt_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // bt_ok
+            // 
+            this.bt_ok.Location = new System.Drawing.Point(388, 100);
+            this.bt_ok.Name = "bt_ok";
+            this.bt_ok.Size = new System.Drawing.Size(75, 23);
+            this.bt_ok.TabIndex = 5;
+            this.bt_ok.Text = "OK";
+            this.bt_ok.UseVisualStyleBackColor = true;
+            // 
+            // bt_cancelar
+            // 
+            this.bt_cancelar.Location = new System.Drawing.Point(388, 129);
+            this.bt_cancelar.Name = "bt_cancelar";
+            this.bt_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.bt_cancelar.TabIndex = 5;
+            this.bt_cancelar.Text = "Cancelar";
+            this.bt_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // bt_pesquisa
+            // 
+            this.bt_pesquisa.Location = new System.Drawing.Point(388, 158);
+            this.bt_pesquisa.Name = "bt_pesquisa";
+            this.bt_pesquisa.Size = new System.Drawing.Size(75, 23);
+            this.bt_pesquisa.TabIndex = 5;
+            this.bt_pesquisa.Text = "Pesquisar";
+            this.bt_pesquisa.UseVisualStyleBackColor = true;
+            // 
+            // bt_imprimir
+            // 
+            this.bt_imprimir.Location = new System.Drawing.Point(388, 187);
+            this.bt_imprimir.Name = "bt_imprimir";
+            this.bt_imprimir.Size = new System.Drawing.Size(75, 23);
+            this.bt_imprimir.TabIndex = 5;
+            this.bt_imprimir.Text = "Imprimir";
+            this.bt_imprimir.UseVisualStyleBackColor = true;
+            // 
+            // bt_sair
+            // 
+            this.bt_sair.Location = new System.Drawing.Point(388, 216);
+            this.bt_sair.Name = "bt_sair";
+            this.bt_sair.Size = new System.Drawing.Size(75, 23);
+            this.bt_sair.TabIndex = 5;
+            this.bt_sair.Text = "Sair";
+            this.bt_sair.UseVisualStyleBackColor = true;
+            // 
+            // CodigoArbitros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 255);
+            this.Controls.Add(this.bt_sair);
+            this.Controls.Add(this.bt_imprimir);
+            this.Controls.Add(this.bt_pesquisa);
+            this.Controls.Add(this.bt_cancelar);
+            this.Controls.Add(this.bt_ok);
+            this.Controls.Add(this.bt_eliminar);
+            this.Controls.Add(this.bt_alterar);
+            this.Controls.Add(this.bt_novo);
+            this.Controls.Add(this.tb_numeroArbitros);
+            this.Controls.Add(this.bt_first);
+            this.Controls.Add(this.bt_next);
+            this.Controls.Add(this.bt_previous);
+            this.Controls.Add(this.bt_last);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CodigoArbitros";
+            this.Text = "Ficha de Árbitros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bt_last;
+        private System.Windows.Forms.Button bt_previous;
+        private System.Windows.Forms.Button bt_next;
+        private System.Windows.Forms.Button bt_first;
+        private System.Windows.Forms.TextBox tb_numeroArbitros;
+        private System.Windows.Forms.Button bt_novo;
+        private System.Windows.Forms.Button bt_alterar;
+        private System.Windows.Forms.Button bt_eliminar;
+        private System.Windows.Forms.Button bt_ok;
+        private System.Windows.Forms.Button bt_cancelar;
+        private System.Windows.Forms.Button bt_pesquisa;
+        private System.Windows.Forms.Button bt_imprimir;
+        private System.Windows.Forms.Button bt_sair;
+    }
+}
---
+++ PrototipoFuncionalClubeDeJudoDragao/Tabelas/CodigoEntidades.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace PrototipoFuncionalClubeDeJudoDragao.Tabelas
+{
+    partial class CodigoEntidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.bt_sair = new System.Windows.Forms.Button();
+            this.bt_imprimir = new System.Windows.Forms.Button();
+            this.bt_pesquisa = new System.Windows.Forms.Button();
+            this.bt_cancelar = new System.Windows.Forms.Button();
+            this.bt_ok = new System.Windows.Forms.Button();
+            this.bt_eliminar = new System.Windows.Forms.Button();
+            this.bt_alterar = new System.Windows.Forms.Button();
+            this.bt_novo = new System.Windows.Forms.Button();
+            this.tb_numeroEntidades = new System.Windows.Forms.TextBox();
+            this.bt_first = new System.Windows.Forms.Button();
+            this.bt_next = new System.Windows.Forms.Button();
+            this.bt_previous = new System.Windows.Forms.Button();
+            this.bt_last = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Código de Entidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Descrção";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Morada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Localidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Código-Postal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Telefone/Fax";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Treinador/Contato";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Tipo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(315, 20);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(315, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(118, 148);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(315, 20);
+            this.textBox4.TabIndex = 20;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(118, 174);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(315, 20);
+            this.textBox5.TabIndex = 20;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 200);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(315, 20);
+            this.textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(118, 226);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(315, 20);
+            this.textBox7.TabIndex = 20;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(118, 22);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(54, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // bt_sair
+            // 
+            this.bt_sair.Location = new System.Drawing.Point(450, 228);
+            this.bt_sair.Name = "bt_sair";
+            this.bt_sair.Size = new System.Drawing.Size(75, 23);
+            this.bt_sair.TabIndex = 21;
+            this.bt_sair.Text = "Sair";
+            this.bt_sair.UseVisualStyleBackColor = true;
+            // 
+            // bt_imprimir
+            // 
+            this.bt_imprimir.Location = new System.Drawing.Point(450, 199);
+            this.bt_imprimir.Name = "bt_imprimir";
+            this.bt_imprimir.Size = new System.Drawing.Size(75, 23);
+            this.bt_imprimir.TabIndex = 22;
+            this.bt_imprimir.Text = "Imprimir";
+            this.bt_imprimir.UseVisualStyleBackColor = true;
+            // 
+            // bt_pesquisa
+            // 
+            this.bt_pesquisa.Location = new System.Drawing.Point(450, 170);
+            this.bt_pesquisa.Name = "bt_pesquisa";
+            this.bt_pesquisa.Size = new System.Drawing.Size(75, 23);
+            this.bt_pesquisa.TabIndex = 23;
+            this.bt_pesquisa.Text = "Pesquisar";
+            this.bt_pesquisa.UseVisualStyleBackColor = true;
+            // 
+            // bt_cancelar
+            // 
+            this.bt_cancelar.Location = new System.Drawing.Point(450, 141);
+            this.bt_cancelar.Name = "bt_cancelar";
+            this.bt_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.bt_cancelar.TabIndex = 24;
+            this.bt_cancelar.Text = "Cancelar";
+            this.bt_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // bt_ok
+            // 
+            this.bt_ok.Location = new System.Drawing.Point(450, 112);
+            this.bt_ok.Name = "bt_ok";
+            this.bt_ok.Size = new System.Drawing.Size(75, 23);
+            this.bt_ok.TabIndex = 25;
+            this.bt_ok.Text = "OK";
+            this.bt_ok.UseVisualStyleBackColor = true;
+            // 
+            // bt_eliminar
+            // 
+            this.bt_eliminar.Location = new System.Drawing.Point(450, 83);
+            this.bt_eliminar.Name = "bt_eliminar";
+            this.bt_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.bt_eliminar.TabIndex = 26;
+            this.bt_eliminar.Text = "Eliminar";
+            this.bt_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // bt_alterar
+            // 
+            this.bt_alterar.Location = new System.Drawing.Point(450, 54);
+            this.bt_alterar.Name = "bt_alterar";
+            this.bt_alterar.Size = new System.Drawing.Size(75, 23);
+            this.bt_alterar.TabIndex = 27;
+            this.bt_alterar.Text = "Alterar";
+            this.bt_alterar.UseVisualStyleBackColor = true;
+            // 
+            // bt_novo
+            // 
+            this.bt_novo.Location = new System.Drawing.Point(450, 25);
+            this.bt_novo.Name = "bt_novo";
+            this.bt_novo.Size = new System.Drawing.Size(75, 23);
+            this.bt_novo.TabIndex = 28;
+            this.bt_novo.Text = "Novo";
+            this.bt_novo.UseVisualStyleBackColor = true;
+            // 
+            // tb_numeroEntidades
+            // 
+            this.tb_numeroEntidades.Location = new System.Drawing.Point(300, 266);
+            this.tb_numeroEntidades.Name = "tb_numeroEntidades";
+            this.tb_numeroEntidades.Size = new System.Drawing.Size(55, 20);
+            this.tb_numeroEntidades.TabIndex = 33;
+            // 
+            // bt_first
+            // 
+            this.bt_first.Location = new System.Drawing.Point(228, 265);
+            this.bt_first.Name = "bt_first";
+            this.bt_first.Size = new System.Drawing.Size(65, 22);
+            this.bt_first.TabIndex = 29;
+            this.bt_first.Text = "Primeiro";
+            this.bt_first.UseVisualStyleBackColor = true;
+            // 
+            // bt_next
+            // 
+            this.bt_next.Location = new System.Drawing.Point(157, 264);
+            this.bt_next.Name = "bt_next";
+            this.bt_next.Size = new System.Drawing.Size(65, 23);
+            this.bt_next.TabIndex = 30;
+            this.bt_next.Text = "Seguinte";
+            this.bt_next.UseVisualStyleBackColor = true;
+            // 
+            // bt_previous
+            // 
+            this.bt_previous.Location = new System.Drawing.Point(86, 264);
+            this.bt_previous.Name = "bt_previous";
+            this.bt_previous.Size = new System.Drawing.Size(65, 23);
+            this.bt_previous.TabIndex = 31;
+            this.bt_previous.Text = "Anterior";
+            this.bt_previous.UseVisualStyleBackColor = true;
+            // 
+            // bt_last
+            // 
+            this.bt_last.Location = new System.Drawing.Point(15, 264);
+            this.bt_last.Name = "bt_last";
+            this.bt_last.Size = new System.Drawing.Size(65, 23);
+            this.bt_last.TabIndex = 32;
+            this.bt_last.Text = "Último";
+            this.bt_last.UseVisualStyleBackColor = true;
+            // 
+            // CodigoEntidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 371);
+            this.Controls.Add(this.tb_numeroEntidades);
+            this.Controls.Add(this.bt_first);
+            this.Controls.Add(this.bt_next);
+            this.Controls.Add(this.bt_previous);
+            this.Controls.Add(this.bt_last);
+            this.Controls.Add(this.bt_sair);
+            this.Controls.Add(this.bt_imprimir);
+            this.Controls.Add(this.bt_pesquisa);
+            this.Controls.Add(this.bt_cancelar);
+            this.Controls.Add(this.bt_ok);
+            this.Controls.Add(this.bt_eliminar);
+            this.Controls.Add(this.bt_alterar);
+            this.Controls.Add(this.bt_novo);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "CodigoEntidades";
+            this.Text = "Tipo de Entidades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button bt_sair;
+        private System.Windows.Forms.Button bt_imprimir;
+        private System.Windows.Forms.Button bt_pesquisa;
+        private System.Windows.Forms.Button bt_cancelar;
+        private System.Windows.Forms.Button bt_ok;
+        private System.Windows.Forms.Button bt_eliminar;
+        private System.Windows.Forms.Button bt_alterar;
+        private System.Windows.Forms.Button bt_novo;
+        private System.Windows.Forms.TextBox tb_numeroEntidades;
+        private System.Windows.Forms.Button bt_first;
+        private System.Windows.Forms.Button bt_next;
+        private System.Windows.Forms.Button bt_previous;
+        private System.Windows.Forms.Button bt_last;
+    }
+}
