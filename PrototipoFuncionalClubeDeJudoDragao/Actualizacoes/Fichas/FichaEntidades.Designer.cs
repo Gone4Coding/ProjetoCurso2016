66cfa82258a05f773e70b675b19adb3ba4840ae5
Gone4Coding <gongas.gordalina14@gmail.com>
2016-07-08 13:00:54 +0100

    Design das fichas todo done

---
+++ PrototipoFuncionalClubeDeJudoDragao/Actualizacoes/Fichas/FichaEntidades.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace PrototipoFuncionalClubeDeJudoDragao.Actualizacoes.Fichas
+{
+    partial class FichaEntidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_codigoPostal = new System.Windows.Forms.TextBox();
+            this.tb_localidade = new System.Windows.Forms.TextBox();
+            this.tb_morada = new System.Windows.Forms.TextBox();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_codigoEntidade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_numeroFax = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_numeroTelefone = new System.Windows.Forms.TextBox();
+            this.tb_tipo = new System.Windows.Forms.TextBox();
+            this.tb_telefoneContato = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.numeroEntidade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.data = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.descricao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.valor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bt_eliminar = new System.Windows.Forms.Button();
+            this.bt_alterar = new System.Windows.Forms.Button();
+            this.bt_novo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_codigoPostal
+            // 
+            this.tb_codigoPostal.Location = new System.Drawing.Point(112, 132);
+            this.tb_codigoPostal.Name = "tb_codigoPostal";
+            this.tb_codigoPostal.Size = new System.Drawing.Size(401, 20);
+            this.tb_codigoPostal.TabIndex = 17;
+            // 
+            // tb_localidade
+            // 
+            this.tb_localidade.Location = new System.Drawing.Point(112, 106);
+            this.tb_localidade.Name = "tb_localidade";
+            this.tb_localidade.Size = new System.Drawing.Size(401, 20);
+            this.tb_localidade.TabIndex = 18;
+            // 
+            // tb_morada
+            // 
+            this.tb_morada.Location = new System.Drawing.Point(112, 80);
+            this.tb_morada.Name = "tb_morada";
+            this.tb_morada.Size = new System.Drawing.Size(401, 20);
+            this.tb_morada.TabIndex = 19;
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.Location = new System.Drawing.Point(112, 54);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(401, 20);
+            this.tb_nome.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Código-Postal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Localidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Morada";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nome";
+            // 
+            // tb_codigoEntidade
+            // 
+            this.tb_codigoEntidade.Location = new System.Drawing.Point(112, 28);
+            this.tb_codigoEntidade.Name = "tb_codigoEntidade";
+            this.tb_codigoEntidade.Size = new System.Drawing.Size(102, 20);
+            this.tb_codigoEntidade.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Nº Telefone";
+            // 
+            // tb_numeroFax
+            // 
+            this.tb_numeroFax.Location = new System.Drawing.Point(112, 158);
+            this.tb_numeroFax.Name = "tb_numeroFax";
+            this.tb_numeroFax.Size = new System.Drawing.Size(102, 20);
+            this.tb_numeroFax.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nº Fax";
+            // 
+            // tb_numeroTelefone
+            // 
+            this.tb_numeroTelefone.Location = new System.Drawing.Point(267, 158);
+            this.tb_numeroTelefone.Name = "tb_numeroTelefone";
+            this.tb_numeroTelefone.Size = new System.Drawing.Size(102, 20);
+            this.tb_numeroTelefone.TabIndex = 17;
+            // 
+            // tb_tipo
+            // 
+            this.tb_tipo.Location = new System.Drawing.Point(112, 184);
+            this.tb_tipo.Name = "tb_tipo";
+            this.tb_tipo.Size = new System.Drawing.Size(149, 20);
+            this.tb_tipo.TabIndex = 17;
+            // 
+            // tb_telefoneContato
+            // 
+            this.tb_telefoneContato.Location = new System.Drawing.Point(112, 210);
+            this.tb_telefoneContato.Name = "tb_telefoneContato";
+            this.tb_telefoneContato.Size = new System.Drawing.Size(149, 20);
+            this.tb_telefoneContato.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 187);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Tipo";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 213);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Treinador/Contato";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.numeroEntidade,
+            this.data,
+            this.descricao,
+            this.tipo,
+            this.valor});
+            this.listView1.Location = new System.Drawing.Point(12, 236);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(501, 188);
+            this.listView1.TabIndex = 21;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // numeroEntidade
+            // 
+            this.numeroEntidade.Text = "Nº Entidade";
+            this.numeroEntidade.Width = 77;
+            // 
+            // data
+            // 
+            this.data.Text = "Data";
+            this.data.Width = 66;
+            // 
+            // descricao
+            // 
+            this.descricao.Text = "Descrição";
+            this.descricao.Width = 232;
+            // 
+            // tipo
+            // 
+            this.tipo.Text = "Tipo";
+            // 
+            // valor
+            // 
+            this.valor.Text = "Valor";
+            // 
+            // bt_eliminar
+            // 
+            this.bt_eliminar.Location = new System.Drawing.Point(174, 430);
+            this.bt_eliminar.Name = "bt_eliminar";
+            this.bt_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.bt_eliminar.TabIndex = 65;
+            this.bt_eliminar.Text = "Eliminar";
+            this.bt_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // bt_alterar
+            // 
+            this.bt_alterar.Location = new System.Drawing.Point(93, 430);
+            this.bt_alterar.Name = "bt_alterar";
+            this.bt_alterar.Size = new System.Drawing.Size(75, 23);
+            this.bt_alterar.TabIndex = 66;
+            this.bt_alterar.Text = "Alterar";
+            this.bt_alterar.UseVisualStyleBackColor = true;
+            // 
+            // bt_novo
+            // 
+            this.bt_novo.Location = new System.Drawing.Point(12, 430);
+            this.bt_novo.Name = "bt_novo";
+            this.bt_novo.Size = new System.Drawing.Size(75, 23);
+            this.bt_novo.TabIndex = 67;
+            this.bt_novo.Text = "Novo";
+            this.bt_novo.UseVisualStyleBackColor = true;
+            // 
+            // FichaEntidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 471);
+            this.Controls.Add(this.bt_eliminar);
+            this.Controls.Add(this.bt_alterar);
+            this.Controls.Add(this.bt_novo);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.tb_numeroTelefone);
+            this.Controls.Add(this.tb_numeroFax);
+            this.Controls.Add(this.tb_telefoneContato);
+            this.Controls.Add(this.tb_tipo);
+            this.Controls.Add(this.tb_codigoPostal);
+            this.Controls.Add(this.tb_localidade);
+            this.Controls.Add(this.tb_morada);
+            this.Controls.Add(this.tb_codigoEntidade);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Name = "FichaEntidades";
+            this.Text = "FichaEntidades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_codigoPostal;
+        private System.Windows.Forms.TextBox tb_localidade;
+        private System.Windows.Forms.TextBox tb_morada;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_codigoEntidade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_numeroFax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_numeroTelefone;
+        private System.Windows.Forms.TextBox tb_tipo;
+        private System.Windows.Forms.TextBox tb_telefoneContato;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader numeroEntidade;
+        private System.Windows.Forms.ColumnHeader data;
+        private System.Windows.Forms.ColumnHeader descricao;
+        private System.Windows.Forms.ColumnHeader tipo;
+        private System.Windows.Forms.ColumnHeader valor;
+        private System.Windows.Forms.Button bt_eliminar;
+        private System.Windows.Forms.Button bt_alterar;
+        private System.Windows.Forms.Button bt_novo;
+    }
+}
---
+++ PrototipoFuncionalClubeDeJudoDragao/Actualizacoes/Fichas/FichaEntidades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrototipoFuncionalClubeDeJudoDragao.Actualizacoes.Fichas
+{
+    public partial class FichaEntidades : Form
+    {
+        public FichaEntidades()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
