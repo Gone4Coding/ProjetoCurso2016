7e0428c9abcbfa124a2fc68074d4c7e1f46550de
Gone4Coding <gongas.gordalina14@gmail.com>
2016-07-27 19:15:07 +0100

    oh yeah fuck

--- PrototipoFuncionalClubeDeJudoDragao/ModelDiagramaBD.edmx.sql
+++ PrototipoFuncionalClubeDeJudoDragao/ModelDiagramaBD.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 07/04/2016 20:19:17
--- Generated from EDMX file: C:\Users\FearlessJr\Source\Repos\ProjectoCurso2016\PrototipoFuncionalClubeDeJudoDragao\PrototipoFuncionalClubeDeJudoDragao\ModelDiagramaBD.edmx
+-- Date Created: 07/27/2016 17:41:37
+-- Generated from EDMX file: C:\Git\ProjetoCurso2016\PrototipoFuncionalClubeDeJudoDragao\ModelDiagramaBD.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
@@ -37,6 +37,12 @@ CREATE TABLE [dbo].[AtletaSet] (
 );
 GO
 
+-- Creating table 'sociosSet'
+CREATE TABLE [dbo].[sociosSet] (
+    [Id] int IDENTITY(1,1) NOT NULL
+);
+GO
+
 -- --------------------------------------------------
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
@@ -44,6 +50,12 @@ GO
 -- Creating primary key on [Id] in table 'AtletaSet'
 ALTER TABLE [dbo].[AtletaSet]
 ADD CONSTRAINT [PK_AtletaSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'sociosSet'
+ALTER TABLE [dbo].[sociosSet]
+ADD CONSTRAINT [PK_sociosSet]
     PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
